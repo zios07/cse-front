6b3c0bacff126f9c9ba5a1a88b4176b070029c7e
ZiOS <zakaria.kaoukab@gmail.com>
2018-08-03 22:08:43 +0100

    fix server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/soccer-front'));
+app.use(express.static(__dirname + '/dist/cse-site'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname + '/dist/soccer-front/index.html'));
+	res.sendFile(path.join(__dirname + '/dist/cse-site/index.html'));
 })
 
 console.log('Express server listening!');
