b5803e0b6ef58870b413a27528b7fa3a1e5f3160
ZiOS <zakaria.kaoukab@gmail.com>
2018-09-20 23:46:58 +0100

    back to simple bootstrap table

--- src/app/pages/admin/properties/property-list/property-list.component.ts
+++ src/app/pages/admin/properties/property-list/property-list.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Property } from '../../../../domain/property';
 import { EntityService } from '../../../../services/entity.service';
-import { DataTableResource } from '../../../../../../node_modules/angular5-data-table';
-import {} from 'rxjs/Subject';
 
 @Component({
   selector: 'cse-property-list',
@@ -13,10 +11,7 @@ export class PropertyListComponent implements OnInit {
 
   properties: Property[] = [];
   page = 0;
-  size = 10
-	tableResource: DataTableResource<Property>;
-	items: Property[] = [];
-	itemsCount: number;;
+  size = 10;
 
   constructor(private entityService: EntityService) { }
 
@@ -27,28 +22,10 @@ export class PropertyListComponent implements OnInit {
   loadProperties() {
     this.entityService.setPath("properties");
     this.entityService.getAll(this.page, this.size).subscribe((resp: any) => {
-      this.initializeDataTable(resp.content);
+      this.properties = resp.content;
     }, error => {
       console.log(error);
     })
   }
-
-  deleteProperty(id) {
-    console.log(id);
-  }
-
-  reloadItems(params) {
-		if(this.tableResource)
-			this.tableResource.query(params)
-				.then(items => this.items = items);
-	}
-
-	initializeDataTable(properties: Property[]) {
-		this.tableResource = new DataTableResource(properties);
-		this.tableResource.query({offset: 0})
-			.then(items => this.items = items);
-		this.tableResource.count()
-			.then(count => this.itemsCount = count);
-	}
 
 }
